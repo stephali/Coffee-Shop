15c7b985b2adc38c391484896b180142c82768ff
Stephanie Ali <sali.1444@eagle.fgcu.edu>
2022-11-18 14:04:27 -0500

    Initial commit

---
+++ CoffeeShop.cpp
@@ -0,0 +1,21 @@
+/*
+Coffee Shop Simulation
+By: Stephanie Ali
+Course: Software Design Patterns
+Description: This is a simulation of a Coffee Shop ordering system.
+            The cashier first greets the customer and asks what they would like to order.
+            The customer tells them their choice of drink.
+            Then the cashier asks for their name and tells them their grand total.
+            In the end the barista gives out their drink.
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    cout << "Welcome to Starbucks! What can I get started for you today?\n";
+    string drinkChoice;
+    cin >> drinkChoice;
+    return 0;
+}
