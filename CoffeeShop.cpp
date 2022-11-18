7ff08a4b1a94d91ff057836e85de29c56703624f
Stephanie Ali <sali.1444@eagle.fgcu.edu>
2022-11-18 15:51:55 -0500

    This is my second commit.

--- CoffeeShop.cpp
+++ CoffeeShop.cpp
@@ -3,10 +3,10 @@ Coffee Shop Simulation
 By: Stephanie Ali
 Course: Software Design Patterns
 Description: This is a simulation of a Coffee Shop ordering system.
-            The cashier first greets the customer and asks what they would like to order.
-            The customer tells them their choice of drink.
-            Then the cashier asks for their name and tells them their grand total.
-            In the end the barista gives out their drink.
+             The cashier first greets the customer and asks the customer what they would like to order.
+             The customer tells them their choice of drink.
+             The program calculates the total price of the drink.
+             The cashier asks for their name and tells them their grand total.
  */
 
 #include <iostream>
@@ -14,8 +14,28 @@ Description: This is a simulation of a Coffee Shop ordering system.
 using namespace std;
 
 int main(){
+    // Display menu
+    cout << "--Menu--\n" <<
+    "Brewed Coffees: House Blend, Dark Roast, Blonde Roast\n" <<
+    "Lattes: Espresso\n" <<
+    "Milks: Whole, Oat\n" <<
+    "Flavors: Caramel, Vanilla\n" <<
+    "Toppings: Whip, Cinnamon\n\n"
+    ;
+
+    // Take customer's order
     cout << "Welcome to Starbucks! What can I get started for you today?\n";
     string drinkChoice;
     cin >> drinkChoice;
+
+    // Ask for customer's name
+    cout << "You ordered a " << drinkChoice << ". May I have your name?\n";
+    string customerName;
+    cin >> customerName;
+
+    // Give customer's total
+    double total = 5.00;
+    cout << "Thank you " << customerName << ". Your total is $" << total << ".\n";
+
     return 0;
 }
