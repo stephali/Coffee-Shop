64f2893811ad3e0582aa114c02b356671db038d7
stephanieali <sali.1059@gmail.com>
2022-12-11 19:51:14 -0500

    Complete project

--- CoffeeShop.cpp
+++ CoffeeShop.cpp
@@ -1,41 +1,330 @@
-/*
-Coffee Shop Simulation
-By: Stephanie Ali
-Course: Software Design Patterns
-Description: This is a simulation of a Coffee Shop ordering system.
-             The cashier first greets the customer and asks the customer what they would like to order.
-             The customer tells them their choice of drink.
-             The program calculates the total price of the drink.
-             The cashier asks for their name and tells them their grand total.
- */
+/**
+* Coffee Shop Simulation
+* By: Stephanie Ali
+* Course: Software Design Patterns
+* Description: This is a simulation of a Coffee Shop ordering system.
+*              The cashier first greets the customer and asks the customer what they would like to order.
+*              The customer tells them their choice of drink.
+*              The program calculates the total price of the drink.
+*              The cashier asks for their name and tells them their grand total.
+**/
 
 #include <iostream>
 #include <string>
 using namespace std;
 
-int main(){
-    // Display menu
-    cout << "--Menu--\n" <<
-    "Brewed Coffees: House Blend, Dark Roast, Blonde Roast\n" <<
-    "Lattes: Espresso\n" <<
-    "Milks: Whole, Oat\n" <<
-    "Flavors: Caramel, Vanilla\n" <<
-    "Toppings: Whip, Cinnamon\n\n"
-    ;
+//------Component----//
+class Drink {
+public:
+    virtual ~Drink() {}
+    virtual std::string Operation() const = 0;
+    virtual double Cost() const = 0;
 
-    // Take customer's order
+};
+
+//------Concrete Components----//
+class HouseBlend : public Drink {
+public:
+    std::string Operation() const override {
+        return "HouseBlend with";
+    }
+    double Cost() const override {
+        return 2.00;
+    }
+};
+
+class DarkRoast : public Drink {
+public:
+    std::string Operation() const override {
+        return "DarkRoast with";
+    }
+    double Cost() const override {
+        return 2.00;
+    }
+};
+
+class BlondeRoast : public Drink {
+public:
+    std::string Operation() const override {
+        return "BlondeRoast with";
+    }
+    double Cost() const override {
+        return 2.00;
+    }
+};
+
+class Espresso : public Drink {
+public:
+    std::string Operation() const override {
+        return "Espresso with";
+    }
+    double Cost() const override {
+        return 1.50;
+    }
+};
+
+//------Decorator----//
+class CondimentDecorator : public Drink {
+protected:
+    Drink* drink_;
+
+public:
+    CondimentDecorator(Drink* drink) : drink_(drink) {
+    }
+    std::string Operation() const override {
+        return this->drink_->Operation();
+    }
+    double Cost() const override {
+        return this->drink_->Cost();
+    }
+};
+
+//------Concrete Decorators----//
+class Caramel : public CondimentDecorator {
+public:
+    Caramel(Drink* drink) : CondimentDecorator(drink) {
+    }
+    std::string Operation() const override {
+        return CondimentDecorator::Operation() + " Caramel,";
+
+    }
+    double Cost() const override {
+        return CondimentDecorator::Cost() + 0.20;
+
+    }
+};
+
+class Vanilla : public CondimentDecorator {
+public:
+    Vanilla(Drink* drink) : CondimentDecorator(drink) {
+    }
+
+    std::string Operation() const override {
+        return CondimentDecorator::Operation() + " Vanilla,";
+    }
+    double Cost() const override {
+        return CondimentDecorator::Cost() + 0.20;
+
+    }
+};
+
+class Whip : public CondimentDecorator {
+public:
+    Whip(Drink* drink) : CondimentDecorator(drink) {
+    }
+
+    std::string Operation() const override {
+        return CondimentDecorator::Operation() + " Whip,";
+    }
+    double Cost() const override {
+        return CondimentDecorator::Cost() + 0.30;
+
+    }
+};
+
+class Cinnamon : public CondimentDecorator {
+public:
+    Cinnamon(Drink* drink) : CondimentDecorator(drink) {
+    }
+
+    std::string Operation() const override {
+        return CondimentDecorator::Operation() + " Cinnamon,";
+    }
+    double Cost() const override {
+        return CondimentDecorator::Cost() + 0.20;
+
+    }
+};
+
+class Whole : public CondimentDecorator {
+public:
+    Whole(Drink* drink) : CondimentDecorator(drink) {
+    }
+
+    std::string Operation() const override {
+        return CondimentDecorator::Operation() + " Whole Milk,";
+    }
+    double Cost() const override {
+        return CondimentDecorator::Cost() + 0.50;
+
+    }
+};
+
+class Oat : public CondimentDecorator {
+public:
+    Oat(Drink* drink) : CondimentDecorator(drink) {
+    }
+
+    std::string Operation() const override {
+        return CondimentDecorator::Operation() + " Oat Milk,";
+    }
+    double Cost() const override {
+        return CondimentDecorator::Cost() + 0.50;
+
+    }
+};
+
+//------Client Code----//
+void displayDrink(Drink* drink) {
+    // ...
+    std::cout << "You ordered a: " << drink->Operation();
+    std::cout << "\nYour total is: $" << drink->Cost();
+    // ...
+}
+
+//------Main----//
+int main() {
     cout << "Welcome to Starbucks! What can I get started for you today?\n";
-    string drinkChoice;
-    cin >> drinkChoice;
+    bool running = true;
+    while(running){
+        std::cout << "Coffees: [1]House Blend ($2.00), [2]Espresso ($1.50), [3]Blonde Roast ($2.00), [4]Dark Roast ($2.00)\n";
 
-    // Ask for customer's name
-    cout << "You ordered a " << drinkChoice << ". May I have your name?\n";
-    string customerName;
-    cin >> customerName;
+        int coffeeChoice;
+        int condimentChoice;
+        std::cin >> coffeeChoice;
 
-    // Give customer's total
-    double total = 5.00;
-    cout << "Thank you " << customerName << ". Your total is $" << total << ".\n";
+        if(coffeeChoice == 1){
+            Drink* drink = new HouseBlend;
+            while(true){
+                std::cout << "Condiments: [1]Vanilla ($0.20), [2]Caramel ($0.20), [3]Whip ($0.30), [4]Cinnamon ($0.20), [5]Whole Milk ($0.50), [6]Oat Milk ($0.50), [7]Done\n";
+                std::cin >> condimentChoice;
 
+                if(condimentChoice == 7){
+                    running = false;
+
+                    break;
+                }else if(condimentChoice == 1){
+                    drink = new Vanilla(drink);
+
+                }else if(condimentChoice == 2){
+                    drink = new Caramel(drink);
+
+                }else if(condimentChoice == 3){
+                    drink = new Whip(drink);
+
+                }else if(condimentChoice == 4){
+                    drink = new Cinnamon(drink);
+
+                }else if(condimentChoice == 5){
+                    drink = new Whole(drink);
+
+                }else if(condimentChoice == 6){
+                    drink = new Oat(drink);
+                }else{
+                    std::cout << "Enter a Valid Choice\n";
+
+                }
+            }
+            std::cout << "\n\n";
+            displayDrink(drink);
+
+        }else if(coffeeChoice == 2){
+            Drink* drink = new Espresso;
+            while(true){
+                std::cout << "Condiments: [1]Vanilla ($0.20), [2]Caramel ($0.20), [3]Whip ($0.30), [4]Cinnamon ($0.20), [5]Whole Milk ($0.50), [6]Oat Milk ($0.50), [7]Done\n";
+                std::cin >> condimentChoice;
+
+                if(condimentChoice == 7){
+                    running = false;
+
+                    break;
+                }else if(condimentChoice == 1){
+                    drink = new Vanilla(drink);
+
+                }else if(condimentChoice == 2){
+                    drink = new Caramel(drink);
+
+                }else if(condimentChoice == 3){
+                    drink = new Whip(drink);
+
+                }else if(condimentChoice == 4){
+                    drink = new Cinnamon(drink);
+
+                }else if(condimentChoice == 5){
+                    drink = new Whole(drink);
+
+                }else if(condimentChoice == 6){
+                    drink = new Oat(drink);
+                }else{
+                    std::cout << "Enter a Valid Choice\n";
+
+                }
+            }
+            std::cout << "\n\n";
+            displayDrink(drink);
+
+        }else if(coffeeChoice == 3){
+            Drink* drink = new BlondeRoast;
+            while(true){
+                std::cout << "Condiments: [1]Vanilla ($0.20), [2]Caramel ($0.20), [3]Whip ($0.30), [4]Cinnamon ($0.20), [5]Whole Milk ($0.50), [6]Oat Milk ($0.50), [7]Done\n";
+                std::cin >> condimentChoice;
+
+                if(condimentChoice == 7){
+                    running = false;
+
+                    break;
+                }else if(condimentChoice == 1){
+                    drink = new Vanilla(drink);
+
+                }else if(condimentChoice == 2){
+                    drink = new Caramel(drink);
+
+                }else if(condimentChoice == 3){
+                    drink = new Whip(drink);
+
+                }else if(condimentChoice == 4){
+                    drink = new Cinnamon(drink);
+
+                }else if(condimentChoice == 5){
+                    drink = new Whole(drink);
+
+                }else if(condimentChoice == 6){
+                    drink = new Oat(drink);
+                }else{
+                    std::cout << "Enter a Valid Choice\n";
+
+                }
+            }
+            std::cout << "\n\n";
+            displayDrink(drink);
+
+        }else if(coffeeChoice == 4){
+            Drink* drink = new DarkRoast;
+            while(true){
+                std::cout << "Condiments: [1]Vanilla ($0.20), [2]Caramel ($0.20), [3]Whip ($0.30), [4]Cinnamon ($0.20), [5]Whole Milk ($0.50), [6]Oat Milk ($0.50), [7]Done\n";
+                std::cin >> condimentChoice;
+
+                if(condimentChoice == 7){
+                    running = false;
+                    break;
+                }else if(condimentChoice == 1){
+                    drink = new Vanilla(drink);
+
+                }else if(condimentChoice == 2){
+                    drink = new Caramel(drink);
+
+                }else if(condimentChoice == 3){
+                    drink = new Whip(drink);
+
+                }else if(condimentChoice == 4){
+                    drink = new Cinnamon(drink);
+
+                }else if(condimentChoice == 5){
+                    drink = new Whole(drink);
+
+                }else if(condimentChoice == 6){
+                    drink = new Oat(drink);
+                }else{
+                    std::cout << "Enter a valid choice.\n";
+
+                }
+            }
+            std::cout << "\n\n";
+            displayDrink(drink);
+
+        }else{
+            std::cout << "Enter a Valid Choice\n";
+        }
+    }
     return 0;
 }
